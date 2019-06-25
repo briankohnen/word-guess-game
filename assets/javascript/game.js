accf636c434b44361b229e90efc34e0785e9c280
BrianKohnen <briankohnen@yahoo.com>
2019-06-24 20:23:28 -0500

    bk -- at line 26 created an array to store _ as placeholders for characters in randomWord. at line 49, used .match.index and .splice to cut the_ placeholder and replace with the user's correct guess. Need to figure out how to handle randomWords with multiple of the same characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,18 +2,18 @@
 var wins = 0;
 var guessLeft = 6;
 
-// Variables to point to .html
+// Variables to point to .html text
 var currentWordText = document.getElementById("currentWord");
 var userWinsText = document.getElementById("userWins");
 var guessLeftText = document.getElementById("guessLeft");
 var lettersGuessedText = document.getElementById("lettersGuessed");
 
-// Creating an array to store user's guessed letters
-var allLettersGuessed = [];
-
 // Show Wins / guessLeft on .html
 userWinsText.textContent = wins;
 guessLeftText.textContent = guessLeft;
+
+// Creating an array to store user's guessed letters
+var allLettersGuessed = [];
 
 // Declare Array containing possible words to guess
 var allWords = ["span", "div", "getelementbyid", "function", "method", "object",
@@ -23,26 +23,56 @@ var allWords = ["span", "div", "getelementbyid", "function", "method", "object",
 var randomWord = allWords[Math.floor(Math.random() * allWords.length)];
 console.log(randomWord);
 
+// Creating an array to store _ an equal number of times to randomWord.length
+// and display the _ 's on .html
+var randomWordLetterArray = [];
+
+for (var i = 0; i < randomWord.length; i++) {
+   randomWordLetterArray.push("_ ");
+}
+
+currentWordText.textContent = randomWordLetterArray;
+
 // Key press function
 document.onkeyup = function(event) {
 
-    var userGuess = event.key;
+// set userGuess to key pressed and lower case
+var userGuess = event.key.toLowerCase();
 
-    if (randomWord.match(userGuess)) {
+// conditional statement of whether to run game (guessLeft > 0)        
+    if (guessLeft > 0) {
 
-        currentWordText.textContent = userGuess;
-    
-    } else {
+// if userGuess matches any character of randomWord, continue        
+        if (randomWord.match(userGuess)) {
 
-        allLettersGuessed.push(userGuess);
-        lettersGuessedText.textContent = allLettersGuessed;
-        guessLeft--;
-        guessLeftText.textContent = guessLeft;
+// create var whereLetterIs to store index location of where userGuess matches
+            var whereLetterIs = randomWord.match(userGuess).index;
+            console.log(whereLetterIs);
+
+// use splice method to place userGuess at location in whereLetterIs, and remove previous _
+            randomWordLetterArray.splice(whereLetterIs, 1, userGuess);
+
+// display on .html and log to console
+            currentWordText.textContent = randomWordLetterArray;
+            console.log(randomWordLetterArray);
+
+        } else {
+
+// checks allLettersGuessed for repeat guesses to not include twice
+// if incorrect guess, push guess into allLettersGuessed, and decrement guessLeft   
+            if (allLettersGuessed.includes(userGuess)) { 
+                return;        
+            } else {
+            allLettersGuessed.push(userGuess);
+            lettersGuessedText.textContent = allLettersGuessed;                
+            guessLeft--;
+            guessLeftText.textContent = guessLeft;
+            }
+        }
     }
 
-    //currentWordText.textContent = randomWord;
+    else {
+        lettersGuessedText.textContent = "LOSER";
+    }
 }
-
-//function testCharAt
-
 
