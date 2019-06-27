58daa598c4df45feed300d7bc5acc4bbafe61506
BrianKohnen <briankohnen@yahoo.com>
2019-06-27 10:05:15 -0500

    bk -- created new branch to try a different, probably easier solution

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,42 +2,49 @@
 var wins = 0;
 var guessLeft = 6;
 
+
 // Variables to point to .html text
 var currentWordText = document.getElementById("currentWord");
 var userWinsText = document.getElementById("userWins");
 var guessLeftText = document.getElementById("guessLeft");
 var lettersGuessedText = document.getElementById("lettersGuessed");
 
+
 // Show Wins / guessLeft on .html
 userWinsText.textContent = wins;
 guessLeftText.textContent = guessLeft;
 
-// Creating an array to store user's guessed letters
-var allLettersGuessed = [];
-
 // Declare Array containing possible words to guess
 var allWords = ["span", "div", "getelementbyid", "function", "method", "object",
-                "document", "textcontent"];
+                "document", "textcontent", "console", "log", "onkeyup", "event",
+                "var", "while", "string", "boolean", "number", "array", "doctype",
+                "html", "css", "javascript", "indexof", "push", "includes", "match",
+                "tolowercase", "touppercase", "math", "random", "floor", "ceil", "splice"];
 
 // Choose a word from allWords at random and store in randomWord
 var randomWord = allWords[Math.floor(Math.random() * allWords.length)];
 console.log(randomWord);
 
-// Creating an array to store _ an equal number of times to randomWord.length
-// and display the _ 's on .html
-var randomWordLetterArray = [];
 
+// Creating an array to store " _ " an equal number of times to randomWord.length
+var randomWordLetterArray = [];
+// push " _ " an equal number of times/randomWord.length to previous array
 for (var i = 0; i < randomWord.length; i++) {
    randomWordLetterArray.push(" _ ");
 }
-
+// and display the _ 's on .html
 currentWordText.textContent = randomWordLetterArray;
+
+// Creating an array to store user's guessed letters
+var allLettersGuessed = [];
+
 
 // Key press function
 document.onkeyup = function(event) {
 
 // set userGuess to key pressed and lower case
 var userGuess = event.key.toLowerCase();
+
 
 // conditional statement of whether to run game (guessLeft > 0)        
     if (guessLeft > 0) {
@@ -47,9 +54,13 @@ var userGuess = event.key.toLowerCase();
 
 // create var whereLetterIs to store index location of where userGuess matches
             var whereLetterIs = randomWord.search(userGuess);
+            console.log(whereLetterIs);
 
 // use splice method to place userGuess at location in whereLetterIs, and remove previous _
-            randomWordLetterArray.splice(whereLetterIs, 1, userGuess);
+            randomWordLetterArray.splice(whereLetterIs, 1 , " " + userGuess + " ");
+                if (randomWordLetterArray.includes(" " + userGuess + " ")) {
+                        randomWord.search(userGuess + 1);
+                }
 
 // display on .html and log to console
             currentWordText.textContent = randomWordLetterArray;
@@ -64,7 +75,6 @@ var userGuess = event.key.toLowerCase();
                 randomWord = allWords[Math.floor(Math.random() * allWords.length)];
                 console.log(randomWord);
                 randomWordLetterArray = [];
-
                 for (var i = 0; i < randomWord.length; i++) {
                 randomWordLetterArray.push(" _ ");
                 }
