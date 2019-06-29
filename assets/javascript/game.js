38152cea31f9e3eefaac6da647389781f135a15e
BrianKohnen <briankohnen@yahoo.com>
2019-06-29 11:53:08 -0500

    bk -- created function at line 79 to iterate through random word, properly find the user's guess, and display it on the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // Wins / Stats
 var wins = 0;
-var guessLeft = 6;
+var guessLeft = 8;
 
 
 // Variables to point to .html text
@@ -8,7 +8,6 @@ var currentWordText = document.getElementById("currentWord");
 var userWinsText = document.getElementById("userWins");
 var guessLeftText = document.getElementById("guessLeft");
 var lettersGuessedText = document.getElementById("lettersGuessed");
-
 
 // Show Wins / guessLeft on .html
 userWinsText.textContent = wins;
@@ -30,22 +29,43 @@ console.log(randomWord);
 var randomWordLetterArray = [];
 // push " _ " an equal number of times/randomWord.length to previous array
 for (var i = 0; i < randomWord.length; i++) {
-   randomWordLetterArray.push(" _ ");
+   randomWordLetterArray.push("_");
 }
 // and display the _ 's on .html
-currentWordText.textContent = randomWordLetterArray;
+currentWordText.textContent = randomWordLetterArray.join(" ");
 
 // Creating an array to store user's guessed letters
 var allLettersGuessed = [];
-
-randomWordLetterArray = randomWord.split("");
-            console.log(randomWordLetterArray);
 
 // Key press function
 document.onkeyup = function(event) {
 
 // set userGuess to key pressed and lower case
 var userGuess = event.key.toLowerCase();
+
+// function to reset game on win/store user's guesses
+function winReset() {
+    wins++;
+    userWinsText.textContent = wins;
+        randomWord = allWords[Math.floor(Math.random() * allWords.length)];
+        console.log(randomWord);
+            randomWordLetterArray = [];
+            for (var i = 0; i < randomWord.length; i++) {
+                randomWordLetterArray.push("_");
+                }
+            currentWordText.textContent = randomWordLetterArray.join(" ");
+                guessLeft = 8;
+                guessLeftText.textContent = guessLeft;
+                allLettersGuessed = [];
+                lettersGuessedText.textContent = allLettersGuessed
+            };
+function pushUserGuess() {
+               
+    allLettersGuessed.push(userGuess);
+        lettersGuessedText.textContent = allLettersGuessed.join(" ");              
+            guessLeft--;
+                guessLeftText.textContent = guessLeft;
+            };
 
 
 // conditional statement of whether to run game (guessLeft > 0)        
@@ -54,39 +74,27 @@ var userGuess = event.key.toLowerCase();
 // if userGuess matches any character of randomWord, continue        
         if (randomWord.match(userGuess)) {
 
-// create var whereLetterIs to store index location of where userGuess matches
-            var whereLetterIs = randomWord.indexOf([userGuess]);
 
-// use splice method to place userGuess at location in whereLetterIs, and remove previous _
-            randomWordLetterArray.splice(whereLetterIs, 1 , " " + userGuess + " ");
+// define function to iterate through each letter of randomWord, if userGuess matches the letter, splice it into the _ array at the position of that letter
+                function findAllLetters() {
+                    for(var i = 0; i < randomWord.length; i++) {
+                        if (randomWord[i] === userGuess) {
+                            randomWordLetterArray.splice(i, 1, userGuess);
+                        }
+                    }
+                }
+
+            findAllLetters();
 
 // display on .html and log to console
-            currentWordText.textContent = randomWordLetterArray;
+            currentWordText.textContent = randomWordLetterArray.join(" ");
 
 // conditional statement if randomWordLetterArray does not contain " _ " user wins
-            // if (randomWordLetterArray.includes(" _ ")) {
-            //     return;
-            // } else {
-            //     console.log("you win");
-            //     wins++;
-            //     userWinsText.textContent = wins;
-            //     randomWord = allWords[Math.floor(Math.random() * allWords.length)];
-            //     console.log(randomWord);
-            //     randomWordLetterArray = [];
-            //     for (var i = 0; i < randomWord.length; i++) {
-            //     randomWordLetterArray.push(" _ ");
-            //     }
-
-            //     currentWordText.textContent = randomWordLetterArray;
-
-            //     guessLeft = 6;
-            //     guessLeftText.textContent = guessLeft;
-
-            //     allLettersGuessed = [];
-            //     lettersGuessedText.textContent = allLettersGuessed; 
-
-
-            // }
+            if (randomWordLetterArray.includes("_")) {
+                return;
+            } else {
+                winReset();
+            }
 
         } else {
 
@@ -95,10 +103,7 @@ var userGuess = event.key.toLowerCase();
             if (allLettersGuessed.includes(userGuess)) { 
                 return;        
             } else {
-            allLettersGuessed.push(userGuess);
-            lettersGuessedText.textContent = allLettersGuessed;                
-            guessLeft--;
-            guessLeftText.textContent = guessLeft;
+                pushUserGuess();
             }
         }
     }
